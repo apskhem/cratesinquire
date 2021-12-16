dbd062cccdc525940d22c81845bcc5f11358d102
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-12-16 10:47:40 +0700

    update: minor dark theme with black images

--- src/client/scripts/index.ts
+++ src/client/scripts/index.ts
@@ -7,11 +7,6 @@ if (location.protocol !== "https:" && !/localhost/.test(location.hostname)) {
   location.replace(`https://www.cratesinquire.com${location.pathname}`);
 }
 
-// detct dark mode
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  // dark mode
-}
-
 // when everything is loaded, run this script
 window.onload = () => {
   switch (d3.select("main").attr("id")) {
